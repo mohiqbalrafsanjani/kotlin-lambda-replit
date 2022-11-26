21f6ac4bcebf5a7ba052ef5f82bc5c5a976e8b78
Moh. Iqbal Rafsanjani <iqbal.outlaw@gmail.com>
2022-11-26 15:36:55 +0000

    Initial commit

---
+++ main.kt
@@ -0,0 +1,58 @@
+val trick = {
+  println("No Treats!")
+}
+
+val treat: () -> Unit = {
+  println("Have a treat!")
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+  if(isTrick){
+    return trick
+  } else {
+    if(extraTreat != null){
+    println(extraTreat(5))  
+    }
+    
+    return treat
+  }
+}
+
+fun main(){
+  //val trickFunction = trick
+
+  //trickFunction()
+  //treat()
+
+  val coins: (Int) -> String = {
+    quantity -> "$quantity quarters"
+  }  
+  val cupcake: (Int) -> String = {
+    "Have a cupcake!"
+  }
+  val treatFunction = trickOrTreat(false, coins)  
+  val trickFunction = trickOrTreat(true, cupcake)
+  
+  treatFunction()  
+  trickFunction()
+  
+  // $it default 
+  val coins2: (Int) -> String = {
+    "$it quarters"
+  }
+  
+  val treatFunction2 = trickOrTreat(false, coins2)
+  val trickFunction2 = trickOrTreat(true, null)
+  treatFunction2()
+  trickFunction2()
+
+  val treatFunction3 = trickOrTreat(false){
+    "$it quarters"
+  }
+  treatFunction3()
+
+  repeat(4){
+    treatFunction3()
+  }
+  
+}
